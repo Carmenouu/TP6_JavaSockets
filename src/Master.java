327117bdabd4c1cab4d6abcdf52d563b8749670e
mprevot <carmenprevot@hotmail.fr>
2019-06-28 10:50:15 +0200

    CA MARCHE OMG

--- src/Master.java
+++ src/Master.java
@@ -7,52 +7,78 @@ import java.net.Socket;
 
 public class Master {
 	//Nombre de requêtes = nb de workers
-	static int p = 1 ;
+	static int p = 2 ;
 	
 	
 	public static void main(String[] args){
 		Float serveurs[] = new Float[p];
 		try {
 	
-			     //On se connecte à Wikipedia
-		     Socket sock = new Socket("fr.wikipedia.org", 80);
-		    
-		     for (int i=0; i<p; i++) {
-		    	 String request = "Calcul Pi";
-		    	 
-		    	 //nous créons donc un flux en écriture
-			     BufferedOutputStream bos = new BufferedOutputStream(sock.getOutputStream());
-			     
-			     //nous écrivons notre requête
-			     bos.write(request.getBytes());
-			     //Vu que nous utilisons un buffer, nous devons utiliser la méthode flush
-			     //afin que les données soient bien écrite et envoyées au serveur
-			     bos.flush();
-			     
-			     //On récupère maintenant la réponse du serveur 
-			     //dans un flux, comme pour les fichiers...
-			     BufferedInputStream bis = new BufferedInputStream(sock.getInputStream());
-			     
-			     //Il ne nous reste plus qu'à le lire
-			     String content = "";
-				 int stream;
-				 byte[] b = new byte[1024];
-				 while((stream = bis.read(b)) != -1){
-					 content += new String(b, 0, stream);
-				 }
-				 
-				 serveurs[i] = Float.valueOf(content) ;
-		     }
+			     //On se connecte 
+		     Socket sock = new Socket("localhost", 1024);
+		     Socket sock2 = new Socket("localhost", 1025);
+	    
+	    	 String request = "PI";
+	    	 
+	    	 //nous créons donc un flux en écriture
+		     BufferedOutputStream bos = new BufferedOutputStream(sock.getOutputStream());
+		     BufferedOutputStream bos2 = new BufferedOutputStream(sock2.getOutputStream());
 		     
+		     //nous écrivons notre requête
+		     bos.write(request.getBytes());
+		     bos2.write(request.getBytes());
+		     //Vu que nous utilisons un buffer, nous devons utiliser la méthode flush
+		     //afin que les données soient bien écrite et envoyées au serveur
+		     bos.flush();
+		     bos2.flush();
+		     
+		     //On récupère maintenant la réponse du serveur 
+		     //dans un flux, comme pour les fichiers...
+		     BufferedInputStream bis = new BufferedInputStream(sock.getInputStream());
+		     
+		     
+		     //Il ne nous reste plus qu'à le lire
+		     String response = "";
+		      int stream;
+		      byte[] b = new byte[4096];
+		      stream = bis.read(b);
+		      response = new String(b, 0, stream);      
+		      
+			 
+			 serveurs[0] = Float.valueOf(response) ;
+	  
+		     
+		     
+		     //On récupère maintenant la réponse du serveur 
+		     //dans un flux, comme pour les fichiers...
+		     BufferedInputStream bis2 = new BufferedInputStream(sock2.getInputStream());
+		     
+		     
+		     //Il ne nous reste plus qu'à le lire
+		     String response2 = "";
+		      int stream2;
+		      byte[] b2 = new byte[4096];
+		      stream2 = bis2.read(b2);
+		      response2 = new String(b2, 0, stream2);      
+		      
+			 
+			 serveurs[1] = Float.valueOf(response2) ;
+	  
+			 sock.close();
+		     sock2.close();
 		}
 		
 		 catch (IOException e){
 			 e.printStackTrace();
 		 }
 		
+		double moyenne = 0 ;
 		for (int res=0; res<p; res++) {
 			System.out.println("Worker "+res+" : "+serveurs[res]);
+			moyenne+=serveurs[res] ;
 		}
+		moyenne = moyenne/p ;
+		System.out.println("L'approximation de pi obtenue est : "+moyenne);
 	}
 	          
 }
---
+++ src/Worker1024.java
@@ -0,0 +1,87 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+
+public class Worker1024 {
+	
+	public static void main(String[] args) {
+
+         try {
+            ServerSocket serveur = new ServerSocket(1024);
+            Socket s = serveur.accept() ;
+            
+            try {
+                
+                //Ici, nous n'utilisons pas les mêmes objets que précédemment
+                //Je vous expliquerai pourquoi ensuite
+                PrintWriter writer = new PrintWriter(s.getOutputStream());
+                BufferedInputStream reader = new BufferedInputStream(s.getInputStream());
+                
+                //On attend la demande du client            
+                String response = read(reader);
+                InetSocketAddress remote = (InetSocketAddress)s.getRemoteSocketAddress();
+                
+                //On traite la demande du client en fonction de la commande envoyée
+                String toSend = "";
+                
+                switch(response.toUpperCase()){
+                   case "PI":
+                      toSend = String.valueOf(calculPi());
+                      break;
+                   default : 
+                      toSend = "Commande inconnu !";                     
+                      break;
+                }
+                
+                
+                //On envoie la réponse au client
+                writer.write(toSend);
+                //Il FAUT IMPERATIVEMENT UTILISER flush()
+                //Sinon les données ne seront pas transmises au client
+                //et il attendra indéfiniment
+                writer.flush();
+         } catch(SocketException e){
+             System.err.println("LA CONNEXION A ETE INTERROMPUE ! ");
+          } catch (IOException e) {
+             e.printStackTrace();
+          }        
+         
+      }
+      
+      catch (IOException e) {
+          System.err.println("Le port " + 1024 + " est déjà utilisé ! ");
+       }
+        
+	}
+
+	//Méthode permettant d'estimer pi
+	private static double calculPi() {
+		int nThrows = 100000;
+		double x = 0, y = 0;
+		int nSuccess = 0;
+		for (int i = 1; i <= nThrows; i++) {
+			x = Math.random();
+			y = Math.random();
+			if (x * x + y * y <= 1)
+				nSuccess++;
+		}
+		double value = 4.0 * nSuccess / nThrows;
+		return value;
+	}
+	
+	//La méthode que nous utilisons pour lire les réponses
+	   private static String read(BufferedInputStream reader) throws IOException{      
+	      String response = "";
+	      int stream;
+	      byte[] b = new byte[4096];
+	      stream = reader.read(b);
+	      response = new String(b, 0, stream);
+	      return response;
+	   }
+	      
+}
